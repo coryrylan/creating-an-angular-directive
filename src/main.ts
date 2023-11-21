d9085876be5d072e66d8530438b8682c36336bea
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-20 23:35:50 -0600

    5 files modified, 3 added, 7 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,44 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { SnippetsDirective } from './snippets.directive';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [SnippetsDirective],
+  template: `
+    <h1>Creating an Angular Directive</h1>
 
-import { AppModule } from './app/app.module';
+    <p>
+      <a href="https://coryrylan.com/blog/creating-an-angular-directive">Tutorial at coryrylan.com</a>
+    </p>
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+    <p>Type <code>lorem</code> for lorem ipsum</p>
+    <p>Type <code>zombie</code> for zombie ipsum</p>
+    <p>Type <code>bacon</code> for bacon ipsum</p>
+
+    <textarea [appSnippets]="mySnippets"></textarea>
+  `,
+})
+export class App {
+mySnippets: { name: string, content: string}[];
+  constructor() {
+    this.mySnippets = [
+      {
+        name: 'lorem',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+      {
+        name: 'bacon',
+        content: 'Bacon ipsum dolor amet doner strip steak pastrami, hamburger sirloin spare ribs andouille. Salami drumstick strip steak ground round pork loin pastrami pancetta porchetta andouille pork chop short loin. Beef ground round t-bone shank leberkas flank filet mignon boudin meatball jowl short ribs.'
+      },
+      {
+        name: 'zombie',
+        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+      }
+    ];
   }
-  window['ngRef'] = ref;
+}
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+bootstrapApplication(App);
--- src/snippets.directive.ts
+++ src/snippets.directive.ts
@@ -7,27 +7,28 @@ interface Snippet {
 }
 
 @Directive({
+  standalone: true,
   selector: '[appSnippets]',
   host: {
     '(input)': 'onChange($event)'
   }
 })
 export class SnippetsDirective {
-  @Input('appSnippets') snippetsList: Array<Snippet>;
+  @Input('appSnippets') snippetsList: Snippet[] = [];
   private snippetRegex: RegExp;
 
   constructor() {
     this.snippetRegex = /(?:^|\W)(\w+)(?!\w)`/g;  // Match on given string with a following `
   }
 
-  onChange($event) {
+  onChange($event: any) {
     if ($event.target.value.match(this.snippetRegex) !== null) {
       $event.target.value = this._getValue($event.target.value);
     }
   }
 
   private _getValue(value: string) {
-    let snippets = value.match(this.snippetRegex);
+    let snippets = value.match(this.snippetRegex) as RegExpMatchArray;
     snippets.forEach(snippet => {
       value = value.replace(snippet, this._getSnippetContent(snippet));
     });
@@ -35,7 +36,7 @@ export class SnippetsDirective {
     return value;
   }
 
-  private _getSnippetContent(snippet) {
+  private _getSnippetContent(snippet: string) {
     this.snippetsList.forEach(s => {
       if (s.name.toLowerCase() === snippet.trim().replace('`', '').toLowerCase()) {
         snippet = s.content;
